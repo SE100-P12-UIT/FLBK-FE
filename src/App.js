9e62bb0ab4a79b97dd1e1e033973b27cedc9a4f2
Zoraneko <tuandt1409@gmail.com>
2024-12-26 00:59:18 +0700

    Reapply "add BookFlight and FlightSearch"
    This reverts commit 4e20e96ea02f61ba4fd76defd0c2e240c114ec4d.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { ThemeProvider} from '@mui/material';
 import theme from './app/styles/muiOverride.js';
 import Dashboard from  '../src/app/pages/Employee/Dashboard.js'
 const Home = lazy(() => import('./app/pages/home/Home.js'))
+const FlightSearch = lazy(() => import('./app/pages/search/FlightSearch.js'))
+const BookFlight = lazy(() => import('./app/pages/search/BookFlight.js'))
  
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/Dashboard" element={<Dashboard/>}></Route>
+        <Route path="/Search" element={<FlightSearch />}></Route>
+        <Route path="/Booking" element={<BookFlight />}></Route>
       </Routes>
       </ThemeProvider>
     </Suspense>
